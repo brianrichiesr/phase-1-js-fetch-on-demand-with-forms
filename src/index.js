cc3baa497bedd33cd7ca49cedbb6e6e873ebc251
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-18 21:41:04 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    const title = document.querySelector("#movieDetails h4");
+    const summary = document.querySelector("#movieDetails p");
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("#searchByID");
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => {
+            if (response.ok) {
+                console.log("status", response.status)
+                return response.json();
+            } else if (response.status === "404") {
+                throw ("That is not a valid id for a movie");
+            } else {
+                title.textContent = response.statusText;
+                summary.textContent = "";
+                throw (response.statusText);
+            }
+        })
+        .then(data => {
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+            input.value = "";
+        })
+        .catch(err => console.log(err))
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
